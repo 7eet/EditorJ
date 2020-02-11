21b0284defdedd100a508d75a09bcdf8f6b2c3aa
7eet <raisimranjeetsingh@gmail.com>
2020-02-11 17:29:15 +0530

    added logging instance

--- src/main/java/editor/main/Main.java
+++ src/main/java/editor/main/Main.java
@@ -7,10 +7,14 @@ import javafx.scene.control.MenuBar;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 
 public class Main extends Application {
     private MenuBar menuBar;
     private TextArea textArea;
+    private static final Logger logger = LogManager.getLogger(Main.class);
     public static void main(String... args) {
         launch(args);
     }
@@ -19,10 +23,9 @@ public class Main extends Application {
         Controller controller = new Controller(primaryStage);
         primaryStage.setTitle("EditorJ");
         menuBar = controller.intiMenuBar();
-        // menuBar.setStyle("-fx-background-color: #ebebeb;");
-
+        logger.debug("menubar initialized");
         textArea = controller.addTextArea();
-        textArea.setStyle("text-area-background: blue;");
+        logger.debug("textbar initialized");
         BorderPane borderPane = new BorderPane();
         borderPane.setTop(menuBar);
         borderPane.setCenter(textArea);
