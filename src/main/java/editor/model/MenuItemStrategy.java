308c2d31f2be4c4772daf7f3c94353593c76064c
7eet <raisimranjeetsingh@gmail.com>
2020-02-12 18:03:47 +0530

    added logger and default method for adding extension fileter

--- src/main/java/editor/model/MenuItemStrategy.java
+++ src/main/java/editor/model/MenuItemStrategy.java
@@ -1,5 +1,21 @@
 package editor.model;
+import javafx.stage.FileChooser;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public interface MenuItemStrategy {
+    // logger for all implementing classes
+    Logger logger = LogManager.getLogger(MenuItemStrategy.class);
+
     public void execute();
+
+    // this method return filechooser with added some extension filters
+    default FileChooser addExtensionsAndReturnFileChooser() {
+        FileChooser fileChooser =  new FileChooser();
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Text files","*.txt"),
+                new FileChooser.ExtensionFilter("CSS Files","*.css"),
+                new FileChooser.ExtensionFilter("Html Files","*.htm"),
+                new FileChooser.ExtensionFilter("Java Files","*.java"));
+        return fileChooser;
+    }
 }
