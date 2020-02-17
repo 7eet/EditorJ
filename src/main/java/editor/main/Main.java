7980f6f685a1e7f0a94d3d233f3ecd7879e7c689
7eet <raisimranjeetsingh@gmail.com>
2020-02-17 16:43:19 +0530

    added setStyle

--- src/main/java/editor/main/Main.java
+++ src/main/java/editor/main/Main.java
@@ -34,6 +34,7 @@ public class Main extends Application {
         borderPane.setCenter(textArea);
         Scene scene = new Scene(borderPane,600,700);
         primaryStage.getIcons().add(new Image("file:src/main/resources/logo.png"));
+        scene.getStylesheets().add("file:src/main/java/editor/main/Controller.css");
         primaryStage.setScene(scene);
 
         primaryStage.setOnCloseRequest(e -> controller.alertBox());
